acb977313b44aca6ff040c76b0d377f4bc4cb21f
΅Diego¨ <samanodiego888@gmail.com>
2026-02-16 15:38:46 -0600

    Preparación para PubDev

--- example/android/app/src/main/java/com/chavesgu/scan_example/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package example.android.app.src.main.java.com.chavesgu.scan_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
